b1a432b1b788d4ec674b08fa8b53fd5286390d33
Adithyan PB <118650366+Adithyan88pb@users.noreply.github.com>
2023-07-01 11:03:59 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Import the ScrollReveal library
+
+
+// Create a ScrollReveal instance
+const sr =ScrollReveal({
+  distance: '65px',
+  duration: 2600,
+  delay: 450,
+  reset: true,
+});
+
+// Reveal elements with the class "col"
+sr.reveal(".qq", {
+  delay: 200,
+  origin: 'top',
+});
+
+
+
